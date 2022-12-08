2fcd3ef10038510ef4751556af8577272b3bae90
Joost17 <j.driebergen@hkv.nl>
2022-12-08 17:04:38 +0100

    test aangepast

--- tests/test_columns.py
+++ tests/test_columns.py
@@ -1,6 +1,6 @@
 import pandas as pd
 
 def test_dummy():
-	df = pd.read_csv('FC_test.csv',sep=';')
+    assert 1 == 0
+    df = pd.read_csv('FC_test.csv',sep=';')
     assert df.columns[0] == 'ID'
-	assert df.columns[1] == 'code'
