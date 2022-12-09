dbc12778927672297e7a8e14ae792f51be127f95
Joost17 <j.driebergen@hkv.nl>
2022-12-09 17:01:41 +0100

    commit

--- tests/test_columns.py
+++ tests/test_columns.py
@@ -5,3 +5,7 @@ df_FC = pd.read_csv(r'tests/testfiles/FC_test_processed.csv',sep=';')
 
 def test_unique_sampleID():
     assert df_FC.columns[0] == 'ID'
+
+def test_encoding():
+    encoding = 0
+    assert 0==0
