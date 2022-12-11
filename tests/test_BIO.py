94b0ef71fdf7607097ee5f3b598567b398d72922
Joost17 <j.driebergen@hkv.nl>
2022-12-11 15:36:07 +0100

    test BIO

---
+++ tests/test_BIO.py
@@ -0,0 +1,23 @@
+import pandas as pd
+import chardet
+
+#Read output of preprocessing
+file_BIO        = r'tests/testfiles/Diatomeeen_test.csv'
+df_BIO          = pd.read_csv(file_BIO,sep=';')
+file_BIO_prepro = r'tests/testfiles/Diatomeeen_test_processed.csv'
+df_BIO_prepro   = pd.read_csv(file_BIO_prepro,sep=';')
+
+
+def test_dtypes():
+    df_BIO.dtypes['Leverancier_Code'] == object
+
+def test_encoding():
+    with open(file_BIO_prepro, 'rb') as rawdata:
+        result = chardet.detect(rawdata.read(100000))
+    assert result['encoding'] == 'UTF-8-SIG'
+
+def test_length():
+    assert len(df_BIO) == len(df_BIO_prepro)
+
+def test_sampleID_unique():
+    assert len(df_BIO) == len(df_BIO.drop_duplicates(subset=['Meetobject.code', 'Collectie_Datum','Collectie_Datum','Sample_Id_FEWS']))
