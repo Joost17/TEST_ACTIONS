b8c7a9e85c00126c4575952b3410a6a6bae46f84
Joost17 <j.driebergen@hkv.nl>
2022-12-22 17:31:29 +0100

    test

--- wf_testrun/run_wf_testrun.bat
+++ wf_testrun/run_wf_testrun.bat
@@ -3,16 +3,18 @@ ECHO DRAAI DAARBIJ ALS VOORBEELD DE TAAK ImportPvss
 
 
 SET ROOT=D:/FEWSProjects/TEST_actions/TEST_ACTIONS-ontwikkel/wf_testrun
-SET REGION_HOME=D:/FEWSProjects/TEST_actions/TEST_ACTIONS-ontwikkel
-::SET REGION_HOME=../
+::SET REGION_HOME=..
 
-CD /D %REGION_HOME%
+RMDIR /Q /S %CD%\Config
+RMDIR /Q /S %CD%\localDataStore
+RMDIR /Q /S %CD%\temp
+DEL /Q %CD%\runConfigTest.log
 
-:: KOPIEER DE CONFIG VAN DE APDATA EN GOOI DAAR DE LAATSTE SPOC-FILES OVERHEEN
+XCOPY /E ..\Config %CD%\Config\
 
 ECHO START WORFLOW TESTRUN
-SET WORKFLOWTESTRUN=%REGION_HOME%\Config\WorkflowTestRuns\wftr_EFCIS.xml
-START /WAIT wf_testrun/bin/windows/Delft-FEWSc.exe -Djava.net.useSystemProxies=true -Djava.locale.providers=SPI,JRE -Dlog4j.configurationFile=%REGION_HOME%\log4j2.xml -Dsun.err.encoding=UTF-8 -Dsun.stdout.encoding=UTF-8 -Dregion.home=%REGION_HOME% -DautoRollingBarrel=false -Xmx1024m -Djava.library.path=%ROOT%\bin\windows -Wclasspath.1=%REGION_HOME%\patch.jar -Wclasspath.2=%ROOT%\bin\*.jar -Wmain.class=nl.wldelft.fews.system.workflowtestrun.WorkflowTestRun -Warg.1=%REGION_HOME% -Warg.2=%WORKFLOWTESTRUN%
+SET WORKFLOWTESTRUN=Config\WorkflowTestRuns\wftr_EFCIS.xml
+START /WAIT bin/windows/Delft-FEWSc.exe -Djava.net.useSystemProxies=true -Djava.locale.providers=SPI,JRE -Dlog4j.configurationFile=log4j2.xml -Dsun.err.encoding=UTF-8 -Dsun.stdout.encoding=UTF-8 -Dregion.home=%CD% -DautoRollingBarrel=false -Xmx1024m -Djava.library.path=%ROOT%\bin\windows -Wclasspath.1=%CD%\patch.jar -Wclasspath.2=%ROOT%\bin\*.jar -Wmain.class=nl.wldelft.fews.system.workflowtestrun.WorkflowTestRun -Warg.1=%CD% -Warg.2=%WORKFLOWTESTRUN%
 
 :: DE LOGFILE GAAT NAAR DE SpocScripting\logs FOLDER EN WORDT DAAR GECONTROLEEERD OP FOUTEN
 ::COPY /Y %REGION_HOME%\log.txt L:\SpocScripting\logs\runConfigTest.log
