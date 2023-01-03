d8fd5280c26a73aa48621359f2eddac7edf40ea9
Joost17 <j.driebergen@hkv.nl>
2023-01-03 18:07:39 +0100

    test

--- tests/test_wf_test.py
+++ tests/test_wf_test.py
@@ -1,8 +1,4 @@
 #Read output of preprocessing
-file_BIO        = r'tests/testfiles/Diatomeeen_test.csv'
-df_BIO          = pd.read_csv(file_BIO,sep=';')
-file_BIO_prepro = r'tests/testfiles/Diatomeeen_test_processed.csv'
-df_BIO_prepro   = pd.read_csv(file_BIO_prepro,sep=';')
 
 wftestrunlog = 'wf_testrun/log.txt'
 
